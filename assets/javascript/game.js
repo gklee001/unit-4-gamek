c4a9dc737150058eb105e1fb8d328948e38f8426
Kneeslapper <gloriaklee8@gmail.com>
2019-09-20 22:57:19 -0700

    copied and paste for each div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,25 @@
 //pick a random number between 19 to 120, set in the
 $(document).ready(function () {
+    //set variables and random equations
+    var ruby = Math.floor(Math.random() * 12);
+    console.log(ruby)
+    $("#ruby").text(ruby)
+    //
+    var diamond = Math.floor(Math.random() * 12);
+    console.log(diamond)
+    $("#diamond").text(diamond)
+    //
+    var Canary = Math.floor(Math.random() * 12);
+    console.log(Canary)
+    $("#Canary").text(Canary)
+    //
+    var emerald = Math.floor(Math.random() * 12);
+    console.log(emerald)
+    $("#emerald").text(emerald)
+
+
+
+
     var compGuess = Math.floor(Math.random() * 101) + 19;
     console.log(compGuess)
     $("#computerGuess").text(compGuess)
