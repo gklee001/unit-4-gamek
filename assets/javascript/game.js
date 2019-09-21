5099264059d648b5691461216c251608307b6f36
Kneeslapper <gloriaklee8@gmail.com>
2019-09-20 23:49:42 -0700

    fixed bugs and set points to add +=

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,68 @@ $(document).ready(function () {
     //set variables and random equations
     var ruby = Math.floor(Math.random() * 12);
     console.log(ruby)
-    $("#ruby").text(ruby)
+
     //
     var diamond = Math.floor(Math.random() * 12);
     console.log(diamond)
-    $("#diamond").text(diamond)
+
     //
-    var Canary = Math.floor(Math.random() * 12);
-    console.log(Canary)
-    $("#Canary").text(Canary)
+    var canary = Math.floor(Math.random() * 12);
+    console.log(canary)
+
     //
     var emerald = Math.floor(Math.random() * 12);
     console.log(emerald)
-    $("#emerald").text(emerald)
 
-
+    var win = 0
+    $("#wins").text(win)
+    var lose = 0
+    $("#losses").text(lose)
+    var score = 0;
 
 
     var compGuess = Math.floor(Math.random() * 101) + 19;
     console.log(compGuess)
     $("#computerGuess").text(compGuess)
+
+    $("#ruby").on('click', function () {
+        console.log('ruby click')
+        score += ruby;
+        $("#yourScore").text(score)
+        checkGame();
+    })
+    $("#diamond").on('click', function () {
+        score += diamond;
+        $("#yourScore").text(score)
+        checkGame();
+    })
+    $("#canary").on('click', function () {
+        score += canary;
+        $("#yourScore").text(score)
+        checkGame();
+    })
+    $("#emerald").on('click', function () {
+        score += emerald;
+        $("#yourScore").text(score)
+        checkGame();
+    })
+
+    function checkGame() {
+        if (score === compGuess) {
+            win++;
+            score = 0;
+            compGuess = Math.floor(Math.random() * 101) + 19;
+            $("#wins").text(win)
+            $("#losses").text(lose)
+
+        } else if (score > compGuess) {
+            lose--;
+            score = 0;
+            compGuess = Math.floor(Math.random() * 101) + 19;
+            $("#wins").text(win)
+            $("#losses").text(lose)
+        }
+    }
+
 });
 
