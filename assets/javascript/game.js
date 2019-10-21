5696e813d10576c1aa97f9e204d9d8fd8b488797
Kneeslapper <gloriaklee8@gmail.com>
2019-10-20 18:59:50 -0700

    added some css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,33 +4,36 @@ $(document).ready(function () {
     var ruby = Math.floor(Math.random() * 12);
     console.log(ruby)
 
-    //
+    //set variables and random guess for diamond
     var diamond = Math.floor(Math.random() * 12);
     console.log(diamond)
 
-    //
+    //same thing but for yellow
     var canary = Math.floor(Math.random() * 12);
     console.log(canary)
 
-    //
+    //same but for emerald
     var emerald = Math.floor(Math.random() * 12);
     console.log(emerald)
-
+    //variables for wins and loses, use jquery, with variable that start with 0, then use the dive id and put the text for the variable into those divs
     var win = 0
     $("#wins").text(win)
     var lose = 0
     $("#losses").text(lose)
     var score = 0;
 
-
+    //use a variable that will punch out a random number
     var compGuess = Math.floor(Math.random() * 101) + 19;
     console.log(compGuess)
+    //push the computer guess text into that div
     $("#computerGuess").text(compGuess)
-
+    //to add the clicks for each stone
     $("#ruby").on('click', function () {
         console.log('ruby click')
+        //concatinate and continue adding
         score += ruby;
         $("#yourScore").text(score)
+        //need to do a check
         checkGame();
     })
     $("#diamond").on('click', function () {
@@ -48,22 +51,27 @@ $(document).ready(function () {
         $("#yourScore").text(score)
         checkGame();
     })
-
+    //create a condition to when your guesses do equal the computer guess
     function checkGame() {
         if (score === compGuess) {
+            //add those wins
             win++;
             score = 0;
             compGuess = Math.floor(Math.random() * 101) + 19;
+            //push the text for wins and loses to the correct divs
             $("#wins").text(win)
             $("#losses").text(lose)
 
         } else if (score > compGuess) {
+            //else condition
             lose--;
             score = 0;
             compGuess = Math.floor(Math.random() * 101) + 19;
             $("#wins").text(win)
             $("#losses").text(lose)
-        }
+
+        }//display computer guess also
+        $("#computerGuess").text(compGuess)
     }
 
 });
